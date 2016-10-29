94828f43f4c5d7f5e14b07253a0d9427ef884e52
Leon Jacobs <leonja511@gmail.com>
2016-10-29 15:55:05 +0200

    Add script to quickly generate changelogs

---
+++ development/make-changelogs.sh
@@ -0,0 +1,100 @@
+echo " + SeAT Changelog Generator"
+echo
+
+# Die if there are errors.
+set -e
+
+# -- Configurations --
+#
+# Directories
+CHANGELOGS_DIR="changelogs"
+
+# Repositories Location
+MAIN_SEAT_REPO="."
+API_REPO="packages/eveseat/api"
+CONSOLE_REPO="packages/eveseat/console"
+EVEAPI_REPO="packages/eveseat/eveapi"
+NOTIFICATIONS_REPO="packages/eveseat/notifications"
+SERVICES_REPO="packages/eveseat/services"
+WEB_REPO="packages/eveseat/web"
+
+# -- Setup --
+#
+# Get the current directory. We will use this as a reference
+# when we need to move around the file system
+CURRENT_DIRECTORY=`pwd`
+
+# Check that the prereqs of this script is met. We cant continue
+# without git and composer being available to run.
+prereqs=(git)
+
+for i in "${prereqs[@]}"; do
+   command -v $i >/dev/null 2>&1 || { echo \
+    "I require $i but it's not installed. Aborting." >&2; exit 1; }
+done
+
+# Prepare the changelogs directory.
+echo " + Preparing Changelogs Directory"
+if [[ ! -d "$CURRENT_DIRECTORY/$CHANGELOGS_DIR" ]]; then
+
+    echo " * Unable to find the changelogs directory. Bailing!"
+    exit 1
+fi
+
+echo " + Generating changelogs for $MAIN_SEAT_REPO"
+cd $MAIN_SEAT_REPO
+MAIN_SEAT_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Generating changelogs for $API_REPO"
+cd $API_REPO
+API_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Generating changelogs for $CONSOLE_REPO"
+cd $CONSOLE_REPO
+CONSOLE_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Generating changelogs for $EVEAPI_REPO"
+cd $EVEAPI_REPO
+EVEAPI_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Generating changelogs for $NOTIFICATIONS_REPO"
+cd $NOTIFICATIONS_REPO
+NOTIFICATIONS_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Generating changelogs for $SERVICES_REPO"
+cd $SERVICES_REPO
+SERVICES_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Generating changelogs for $WEB_REPO"
+cd $WEB_REPO
+WEB_CHANGELOG=`git log --oneline --decorate`
+cd $CURRENT_DIRECTORY
+
+echo " + Writing Changelogs"
+cd $CHANGELOGS_DIR
+echo "$MAIN_SEAT_CHANGELOG" > seat.md
+echo "$API_CHANGELOG" > api.md
+echo "$CONSOLE_CHANGELOG" > console.md
+echo "$EVEAPI_CHANGELOG" > eveapi.md
+echo "$NOTIFICATIONS_CHANGELOG" > notifications.md
+echo "$SERVICES_CHANGELOG" > services.md
+echo "$WEB_CHANGELOG" > web.md
+
+echo " + Writing Combined Changelog"
+echo "$MAIN_SEAT_CHANGELOG" > all.md
+echo "$API_CHANGELOG" >> all.md
+echo "$CONSOLE_CHANGELOG" >> all.md
+echo "$EVEAPI_CHANGELOG" >> all.md
+echo "$NOTIFICATIONS_CHANGELOG" >> all.md
+echo "$SERVICES_CHANGELOG" >> all.md
+echo "$WEB_CHANGELOG" >> all.md
+cd $CURRENT_DIRECTORY
+
+echo " + Done"
+
