d02fe3b50894d8561dee7c53decebb6d1e353e0a
Leon Jacobs <leonja511@gmail.com>
2018-03-30 20:06:16 +0200

    Create a new .env file if one does not already exist.

--- docker-compose-dev/eveseat-app/startup.sh
+++ docker-compose-dev/eveseat-app/startup.sh
@@ -2,17 +2,24 @@
 
 cd /var/www/seat
 
+# Ensure we have vendor/ ready
 if [ ! -f /var/www/seat/vendor/autoload.php ]; then
 
-    # first run, lets install the app!
     composer install
+fi
 
-    #sed -i -- 's/DB_USERNAME=seat/DB_USERNAME=root/g' .env
+# Ensure we have .env
+if [ ! -f /var/www/seat/.env ]; then
+
+    cp /var/www/seat/.env.example /var/www/seat/.env
+
+    # Fix up MariaDB and Redis connection info
     sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD=seat/g' .env
     sed -i -- 's/APP_DEBUG=false/APP_DEBUG=true/g' .env
     sed -i -- 's/DB_HOST=127.0.0.1/DB_HOST=mariadb/g' .env
     sed -i -- 's/REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g' .env
 
+    # Generate an app key
     php artisan key:generate
 fi
 
