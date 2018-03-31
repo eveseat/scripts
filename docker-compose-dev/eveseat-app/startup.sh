ff5a2fad738550d6b62af14863a1c1ba955b3695
Leon Jacobs <leonja511@gmail.com>
2018-03-31 19:23:33 +0200

    Fix up storage directory permissions.

--- docker-compose-dev/eveseat-app/startup.sh
+++ docker-compose-dev/eveseat-app/startup.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-cd /var/www/seat
-
-# Ensure we have vendor/ ready
-if [ ! -f /var/www/seat/vendor/autoload.php ]; then
-
-    composer install
-fi
-
-# Ensure we have .env
-if [ ! -f /var/www/seat/.env ]; then
-
-    cp /var/www/seat/.env.example /var/www/seat/.env
+function prepare_env() {
 
     # Fix up MariaDB and Redis connection info
     sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD=seat/g' .env
@@ -21,6 +10,23 @@ if [ ! -f /var/www/seat/.env ]; then
 
     # Generate an app key
     php artisan key:generate
+}
+
+cd /var/www/seat
+
+# Ensure we have vendor/ ready
+if [ ! -f /var/www/seat/vendor/autoload.php ]; then
+
+    composer install
+    chown -R www-data:www-data storage
+    prepare_env
+fi
+
+# Ensure we have .env, if we dont, try and fix it automatically
+if [ ! -f /var/www/seat/.env ]; then
+
+    cp /var/www/seat/.env.example /var/www/seat/.env
+    prepare_env
 fi
 
 # publish new assets
