90bb6d505e4e13b2c88be3b0b7fd7a98e2b4c246
Leon Jacobs <leonja511@gmail.com>
2018-03-31 20:08:20 +0200

    Refactor app startup script and revert bash addition.

--- docker-compose-dev/eveseat-app/startup.sh
+++ docker-compose-dev/eveseat-app/startup.sh
@@ -1,32 +1,26 @@
-#!/bin/bash
-
-function prepare_env() {
-
-    # Fix up MariaDB and Redis connection info
-    sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD=seat/g' .env
-    sed -i -- 's/APP_DEBUG=false/APP_DEBUG=true/g' .env
-    sed -i -- 's/DB_HOST=127.0.0.1/DB_HOST=mariadb/g' .env
-    sed -i -- 's/REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g' .env
-
-    # Generate an app key
-    php artisan key:generate
-}
+#!/bin/sh
 
 cd /var/www/seat
 
 # Ensure we have vendor/ ready
 if [ ! -f /var/www/seat/vendor/autoload.php ]; then
 
-    composer install
     chown -R www-data:www-data storage
-    prepare_env
-fi
 
-# Ensure we have .env, if we dont, try and fix it automatically
-if [ ! -f /var/www/seat/.env ]; then
+    # Ensure we have .env, if we dont, try and fix it automatically
+    if [ ! -f /var/www/seat/.env ]; then
 
-    cp /var/www/seat/.env.example /var/www/seat/.env
-    prepare_env
+        cp /var/www/seat/.env.example /var/www/seat/.env
+
+        # Fix up MariaDB and Redis connection info
+        sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD=seat/g' .env
+        sed -i -- 's/APP_DEBUG=false/APP_DEBUG=true/g' .env
+        sed -i -- 's/DB_HOST=127.0.0.1/DB_HOST=mariadb/g' .env
+        sed -i -- 's/REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g' .env
+    fi
+
+    composer install
+    php artisan key:generate
 fi
 
 # publish new assets
