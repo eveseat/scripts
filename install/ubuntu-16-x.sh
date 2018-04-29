9dd7cc18fc4cc5dd64dfbaf8eb27deb8ef1bfeb5
Leon Jacobs <leonja511@gmail.com>
2018-04-29 20:26:07 +0200

    Prevent python encoding error.

--- install/ubuntu-16-x.sh
+++ install/ubuntu-16-x.sh
@@ -15,7 +15,7 @@ cd /root/
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-add-apt-repository -y ppa:ondrej/php
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 
 echo " * Installing installer dependencies"
