90db0f860d3c1af6336499748f46f5770e35b9c1
Crypta Eve <crypta@crypta.tech>
2020-02-28 13:50:16 +1100

    Change prepare-source.sh to pull v4 branches
    We will need to reverse this once v4 is in master

--- docker-compose-dev/prepare-source.sh
+++ docker-compose-dev/prepare-source.sh
@@ -6,16 +6,16 @@ openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -config ./confi
 
 echo "Pulling sources..."
 
-git clone https://github.com/eveseat/seat && cd seat
+git clone -b 4.0.x https://github.com/eveseat/seat && cd seat
 curl -fsSL https://raw.githubusercontent.com/eveseat/scripts/master/development/composer.dev.json > composer.json
 
 mkdir -p packages/eveseat && cd packages/eveseat
-git clone https://github.com/eveseat/api
-git clone https://github.com/eveseat/console
-git clone https://github.com/eveseat/eveapi
+git clone -b 4.0.x https://github.com/eveseat/api
+git clone -b 4.0.x https://github.com/eveseat/console
+git clone -b 4.0.x https://github.com/eveseat/eveapi
 git clone https://github.com/eveseat/eseye
-git clone https://github.com/eveseat/notifications
-git clone https://github.com/eveseat/services
-git clone https://github.com/eveseat/web
+git clone -b 4.0.x https://github.com/eveseat/notifications
+git clone -b 4.0.x https://github.com/eveseat/services
+git clone -b 4.0.x https://github.com/eveseat/web
 
 echo "Done! docker-compose up now!"
