866f8cf945d4ac9d6ef605e8537527f2455e01f8
Ruud Cools <Kaldie@users.noreply.github.com>
2020-08-30 14:48:56 +0200

    ensure tag is correct and used in setup script (#36)
    Co-authored-by: Ruud Cools <ruud.cools@ns.nl>

--- docker-compose-dev/prepare-source.sh
+++ docker-compose-dev/prepare-source.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BRANCH=4.0.x
+BRANCH=4.0.0
 declare -a BRANCHED_PACKAGES=("api" "console" "eveapi" "notifications" "services" "web")
 declare -a PACKAGES=("eseye")
 
 echo "[i] Pulling parent seat project"
-git clone -b 4.0.x https://github.com/eveseat/seat sources && cd sources
+git clone -b ${BRANCH} https://github.com/eveseat/seat sources && cd sources
 echo "[i] Getting development composer.json"
 curl -fsSL https://raw.githubusercontent.com/eveseat/scripts/master/development/composer.dev.json > composer.json
 
